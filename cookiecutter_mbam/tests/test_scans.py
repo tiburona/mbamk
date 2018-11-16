92307331b99adc2db2e22f47ed6951020b131cde
Katie Surrence <tiburona@gmail.com>
2018-11-16 06:44:56 -0500

    passing three extant scan tests
    Former-commit-id: cdb3cb5e1a6e06fa2d717e6e92cf82ab260f701a
    Former-commit-id: 7e285032983078a5ccf026a14a5756cf7941d342
    Former-commit-id: 81edc66e908eb6996e07bede52d978d1f5da148e

--- cookiecutter_mbam/tests/test_scans.py
+++ cookiecutter_mbam/tests/test_scans.py
@@ -30,10 +30,11 @@ class TestScanUpload:
         _process_file returns a two tuple: (the gzipped file object, False)
         """
 
-        with open('/Users/katie/spiro/cookiecutter_mbam/files/T1.nii', 'rb') as f:
-            file_object, import_service = new_scan_service._process_file(f)
-            assert not import_service
-            assert type(file_object).__name__ == 'GzipFile'
+        with open('/Users/katie/spiro/mbam/cookiecutter_mbam/files/T1.nii', 'rb') as f:
+            file = FileStorage(f)
+        file_object, import_service = new_scan_service._process_file(file)
+        assert not import_service
+        assert type(file_object).__name__ == 'GzipFile'
 
     def test_zip_file(self, new_scan_service, mocker):
         """
@@ -44,25 +45,23 @@ class TestScanUpload:
         3) xnat_put is called with expected args, including imp=True
         """
 
-        with open('/Users/katie/spiro/cookiecutter_mbam/files/DICOM.zip', 'rb') as f:
-            file = FileStorage(f)
-            file.save('/Users/katie/spiro/cookiecutter_mbam/files/DICOM1.zip')
-            print(type(file))
-            print(file.filename)
-
-        print("hey ", type(file))
+        f = open('/Users/katie/spiro/mbam/cookiecutter_mbam/files/DICOM.zip', 'rb')
+        file = FileStorage(f)
+        print(type(file))
         file_object, import_service = new_scan_service._process_file(file)
         assert import_service
         xc = XNATConnection()
         xc.xnat_put = mocker.MagicMock()
         mocker.spy(new_scan_service.xc, 'xnat_put')
         mocker.spy(new_scan_service, '_generate_xnat_identifiers')
-        new_scan_service.upload(f)
+        new_scan_service.upload(file)
         new_scan_service._generate_xnat_identifiers.assert_called_with(dcm=True)
         xnat_ids = new_scan_service._generate_xnat_identifiers(dcm=True)
         assert xnat_ids['resource']['xnat_id'] == 'DICOM'
-        new_scan_service.xc.xnat_put.assert_called_with(file=f, imp=True, project= 'MBAM_TEST',
+        new_scan_service.xc.xnat_put.assert_called_with(file=file, imp=True, project= 'MBAM_TEST',
                                                                    subject='000001', experiment='000001_MR2')
+        f.close(); file.close()
+
 
 
 
@@ -73,7 +72,6 @@ class TestScanUpload:
         Then test that xnat_experiment_id and xnat_scan_id are as expected
         """
 
-        with open('/Users/katie/spiro/cookiecutter_mbam/files/T1.nii.gz', 'rb') as f:
-            xnat_ids = new_scan_service._generate_xnat_identifiers()
-            assert xnat_ids['experiment']['xnat_id'] == '000001_MR2'
-            assert xnat_ids['scan']['xnat_id'] == 'T1_2'
+        xnat_ids = new_scan_service._generate_xnat_identifiers()
+        assert xnat_ids['experiment']['xnat_id'] == '000001_MR2'
+        assert xnat_ids['scan']['xnat_id'] == 'T1_2'
