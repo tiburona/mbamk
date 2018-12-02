17dcc3e5e95fc475fe2e847a4a653f4102cde8c9
Katie Surrence <tiburona@gmail.com>
2018-12-02 04:31:48 -0500

    test refactoring to remove redundancy
    Former-commit-id: 10ae885e716080629e8eb5c16425e10c0cee2b31
    Former-commit-id: fd46c669c49924cdcf930828c43e55f2538a7b6b
    Former-commit-id: 3576c339b1bef6c14fb23907f6c5e58e9efe674f

--- cookiecutter_mbam/tests/test_scans_param.py
+++ cookiecutter_mbam/tests/test_scans_param.py
@@ -17,11 +17,11 @@ import faker
 fake = faker.Faker()
 
 
-def generate_parameters():
+def generate_parameters(num_exp, num_scans):
     test_data = []
     root_path  = '/data/archive/experiments/'
-    for num_exp in range(1,2):
-        for num_scans in range(2):
+    for i in range(1,num_exp+1):
+        for j in range(num_scans):
             exp_id = '000001_MR' + str(num_exp)
             scan_id = 'T1_' + str(num_scans + 1)
             exp_uri = os.path.join(root_path, exp_id)
@@ -30,14 +30,15 @@ def generate_parameters():
     return test_data
 
 
-parameters = generate_parameters()
+parameters = generate_parameters(2, 2)
 
 @pytest.fixture(scope='function')
 @pytest.mark.usefixtures('db')
 def mocked_scan_service(db, mocker, request):
     user = User.create(email='a@b.com', username='hello', password='myprecious')
+    num_exp, num_scans, exp_id, scan_id, exp_uri, scan_uri = request.param
 
-    for num_exp in range(request.param[0]):
+    for i in range(num_exp):
         #experiment = ExperimentFactory(user_id = user.id)
         date = fake.date_this_decade(before_today=True, after_today=False)
         scanner = 'GE'
@@ -45,13 +46,13 @@ def mocked_scan_service(db, mocker, request):
         db.session.add(experiment)
 
     ss = ScanService(experiment.user_id, experiment.id)
-    for num_scan in range(request.param[1]):
+    for i in range(num_scans):
         ss._add_scan_to_database() # should this just be adding a scan via the same method as the experiments are added??
 
     ss.xc.upload_scan = mocker.MagicMock()
     ss.xc.upload_scan.return_value = ('/data/archive/subjects/000001',
-                                      request.param[4],
-                                      request.param[5])
+                                      exp_uri,
+                                      scan_uri)
     mocker.spy(ss, '_generate_xnat_identifiers')
     ss.param = request.param
     return ss
@@ -66,16 +67,22 @@ class TestScanUpload:
         copy(src_path, dst_path)
         return os.path.join(dst_path, basename)
 
-    def setup_test_files(self, scan_service, filename):
+    def create_file_to_upload(self, scan_service, filename):
         path = self.copy_file_to_upload_dest(scan_service.instance_path, filename)
         return open(path, 'rb')
 
     def add_a_scan(self, scan_service, filename):
-        f = self.setup_test_files(scan_service, filename)
-        file = FileStorage(f)
-        scan_service.add(file)
-        f.close()
-        return (scan_service, file)
+        with self.create_file_to_upload(scan_service, filename) as f:
+            file = FileStorage(f)
+            scan_service.add(file)
+            file.close()
+        return scan_service
+
+    def setup_tests(self, scan_service, filename):
+        scan_service = self.add_a_scan(scan_service, filename)
+        retrieved_scans = Scan.query.filter((Scan.experiment_id == scan_service.experiment.id))
+        scan = retrieved_scans.order_by(Scan.created_at.desc()).first()
+        return (scan_service, retrieved_scans, scan)
 
     def test_before_file_upload_an_experiment_has_no_xnat_attributes(self, mocked_scan_service, filename):
         assert mocked_scan_service.experiment.xnat_uri == None
@@ -83,24 +90,20 @@ class TestScanUpload:
 
     def test_after_file_upload_experiment_has_the_right_number_of_scans(self, mocked_scan_service, filename):
         num_exp, num_scans, exp_id, scan_id, exp_uri, scan_uri = mocked_scan_service.param
-        scan_service, file = self.add_a_scan(mocked_scan_service, filename)
-        retrieved_scans = Scan.query.filter((Scan.experiment_id == mocked_scan_service.experiment.id))
+        scan_service, retrieved_scans, _ = self.setup_tests(mocked_scan_service, filename)
         assert retrieved_scans.count() == num_scans + 1
         assert scan_service.experiment.num_scans == num_scans + 1
-        file.close()
 
     def test_after_file_upload_experiment_attributes_are_set(self, mocked_scan_service, filename):
         num_exp, num_scans, exp_id, scan_id, exp_uri, scan_uri = mocked_scan_service.param
-        scan_service, file = self.add_a_scan(mocked_scan_service, filename)
+        scan_service, retrieved_scans, scan = self.setup_tests(mocked_scan_service, filename)
         assert scan_service.experiment.xnat_uri == exp_uri
         assert scan_service.experiment.xnat_experiment_id == exp_id
 
     def test_after_file_upload_scan_attributes_are_set(self, mocked_scan_service, filename):
         num_exp, num_scans, exp_id, scan_id, exp_uri, scan_uri = mocked_scan_service.param
-        scan_service, file = self.add_a_scan(mocked_scan_service, filename)
-        retrieved_scans = Scan.query.filter((Scan.experiment_id == mocked_scan_service.experiment.id))
-        scan = retrieved_scans.order_by(Scan.created_at.desc()).first()
+        scan_service, retrieved_scans, scan = self.setup_tests(mocked_scan_service, filename)
         assert scan.xnat_scan_id == scan_id
         assert scan.xnat_uri == scan_uri
-        file.close()
+
 
