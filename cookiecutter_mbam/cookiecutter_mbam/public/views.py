ea527ef5ef4e6b2575309d0ff6ea777c06b59edb
Spiro Pantazatos <spiropan@gmail.com>
2018-12-28 18:52:16 -0500

    finishing touches for flask_security
    Former-commit-id: 9888772e40ce1fcf2554c4958f341a16b076943f
    Former-commit-id: 7d78e80b8bdb3c088a4d66b020f208633f36c63c
    Former-commit-id: 6a8d00b1cc2ed569cb8fe9f579d517883e1beafc

--- cookiecutter_mbam/cookiecutter_mbam/public/views.py
+++ cookiecutter_mbam/cookiecutter_mbam/public/views.py
@@ -10,20 +10,10 @@ from cookiecutter_mbam.utils import flash_errors
 
 blueprint = Blueprint('public', __name__, static_folder='../static')
 
-@blueprint.route('/', methods=['GET', 'POST'])
+@blueprint.route('/')
 def home():
     """ Home page. """
-    form = LoginForm(request.form)
-    # Handle logging in
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            login_user(form.user)
-            flash('You are logged in.', 'success')
-            redirect_url = request.args.get('next') or url_for('user.members')
-            return redirect(redirect_url)
-        else:
-            flash_errors(form)
-    return render_template('public/home.html', form=form)
+    return render_template('public/home.html')
 
 @blueprint.route('/FAQ')
 def FAQ():
