4702648323cfb86738a336f7e458d8098a82794d
Katie Surrence <tiburona@gmail.com>
2019-01-17 10:55:31 -0500

    improvements to add scan controller methods
    Former-commit-id: 391dcfaa5eceb3d5b43f40c9e8b880cfef35f244
    Former-commit-id: ed55a1a6a2cc786bcf0362c266beda8101a225ba
    Former-commit-id: ccd4adf8a31e912771bd7608acf4a2f70f70793d

--- cookiecutter_mbam/cookiecutter_mbam/scan/views.py
+++ cookiecutter_mbam/cookiecutter_mbam/scan/views.py
@@ -5,6 +5,7 @@ from flask_login import current_user
 from .forms import ScanForm, ExperimentAndScanForm
 from .service import ScanService
 from cookiecutter_mbam.experiment.views import add_experiment
+from cookiecutter_mbam.experiment import Experiment
 from cookiecutter_mbam.utils import flash_errors
 
 blueprint = Blueprint('scan', __name__, url_prefix='/scans', static_folder='../static')
@@ -14,49 +15,63 @@ from flask import current_app
 def debug():
     assert current_app.debug == False, "Don't panic! You're here by request of debug()"
 
-
-def validate_number_of_files(request, failure_redirect):
-    """Ensure number of files uploaded is between 1 and 3, inclusive"""
-    if len(request.files.getlist('scan_file')) > 3:
-        flash('You can upload up to three files.', 'warning')
-        return redirect(url_for(failure_redirect))
-    if len(request.files.getlist('scan_file')) < 1:
-        # This is a workaround for FileRequired() failing inexplicably.  Long-term should fix this.
-        flash('A file is required.', 'warning')
-        return redirect(url_for(failure_redirect))
-
-def add_scan(request, exp_id):
-    """Add a scan"""
+def add_scans(request, exp_id):
+    """Add scan files"""
+    num2words = {
+        1: 'a new scan',
+        2: 'two new scans',
+        3: 'three new scans'
+    }
     user_id = str(current_user.get_id())
     for f in request.files.getlist('scan_file'):
         ScanService(user_id, exp_id).add(f)
-    flash('You successfully added a new scan.', 'success')
+    num_scans = len(request.files.getlist('scan_file'))
+    flash('You successfully added {}.'.format(num2words[num_scans]), 'success')
     return redirect(url_for('experiment.experiments'))
+
+def scan_number_validation(request, add_exp):
+    """Validate that the number of scan files for a given experiment is at least one and no more than three"""
+    num2words = {
+        1: 'one scan',
+        2: 'two scans',
+        3: 'three scans'
+    }
+    num_scans_to_add = len(request.files.getlist('scan_file'))
+    if num_scans_to_add < 1:
+        # This is a workaround for FileRequired() failing inexplicably.  Long-term should fix this.
+        return 'A file is required.'
+    if add_exp:
+        if num_scans_to_add > 3:
+           return 'You can upload up to three files.'
+    else:
+        num_scans = Experiment.get_by_id(str(session['curr_experiment'])).num_scans
+        if num_scans + num_scans_to_add > 3:
+            return "A session can only have three scans.  You already have {}.".format(num2words[num_scans])
+    return ''
+
+def meta_add(form, request, redirect_route, template, add_exp = False):
+    """Validate form, initiate adding experiments and/or scans, display messages to user and redirect"""
+    if form.validate_on_submit():
+        scan_number_error = scan_number_validation(request, add_exp)
+        if len(scan_number_error):
+            flash(scan_number_error, 'warning')
+            return redirect(url_for(redirect_route))
+        if add_exp:
+            exp_id = add_experiment(form)
+        else:
+            exp_id = str(session['curr_experiment'])
+        return add_scans(request, exp_id)
+    else:
+        flash_errors(form)
+    return render_template(template, form=form)
 
 @blueprint.route('/add', methods=['GET', 'POST'])
 def add():
     """Access the add scan route and form."""
-    form = ScanForm()
-    if form.validate_on_submit():
-        wrong_number_of_files = validate_number_of_files(request, 'scan.add_experiment_and_scans')
-        if wrong_number_of_files:
-            return wrong_number_of_files
-        exp_id = str(session['curr_experiment'])
-        return add_scan(request, exp_id)
-    else:
-        flash_errors(form)
-    return render_template('scans/upload.html',scan_form=form)
+    return meta_add(ScanForm(request.form), request, 'scan.add', 'scans/upload.html')
 
 @blueprint.route('/add_experiment_and_scans', methods=['GET', 'POST'])
 def add_experiment_and_scans():
     """Acess the add_experiment_and_scans route and form"""
-    form = ExperimentAndScanForm(request.form)
-    if form.validate_on_submit():
-        wrong_number_of_files = validate_number_of_files(request, 'scan.add_experiment_and_scans')
-        if wrong_number_of_files:
-            return wrong_number_of_files
-        exp_id = add_experiment(form)
-        return add_scan(request, exp_id)
-    else:
-        flash_errors(form)
-    return render_template('scans/experiment_and_scans.html', experiment_and_scan_form=form)
+    return meta_add(ExperimentAndScanForm(request.form), request, 'scan.add_experiment_and_scans',
+                    'scans/experiment_and_scans.html', add_exp=True)
