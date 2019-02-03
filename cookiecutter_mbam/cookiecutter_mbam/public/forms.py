b3a8d7649ed4f8f754e9a43b55f74ad2d80e1d99
Spiro Pantazatos <spiropan@gmail.com>
2019-02-03 01:51:28 -0500

    add contact form
    Former-commit-id: b4382fb4421552e9e1da5aa28f79f0181abf5d8f
    Former-commit-id: db38b7d6cb0e8c10286e62ee18d75199a5867171
    Former-commit-id: a50260e82c3cefb1da9ba408c559e80ce19040c4

--- cookiecutter_mbam/cookiecutter_mbam/public/forms.py
+++ cookiecutter_mbam/cookiecutter_mbam/public/forms.py
@@ -4,6 +4,13 @@ from flask_wtf import FlaskForm
 from flask_security.forms import LoginForm
 from wtforms.fields.html5 import EmailField
 from wtforms import StringField, SelectField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length, Required
+from wtforms.validators import DataRequired, Email, Length, Required, InputRequired
 
 from cookiecutter_mbam.user.models import User
+
+class ContactForm(FlaskForm):
+    """ Contact form. """
+    fullname = StringField('Full Name (required)', [InputRequired()])
+    email = EmailField('Contact Email (required)', [Required(), Email()])
+    subject = SelectField('Subject', choices=[('General Inquiry','General Inquiry'),('Site Suggestion','Site Suggestion'),('Other','Other')])
+    message = TextAreaField('Message (required)', [InputRequired(), Length(max=500)])
