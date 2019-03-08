8da71246ce8ddad885f8a02e55f2c176a0679950
Katie Surrence <tiburona@gmail.com>
2019-03-07 22:28:38 -0500

    made columns stack
    Former-commit-id: 1b185d02f8172178f45dbba5f417ff14c8bd5e38
    Former-commit-id: 9a294e02e1f2b310fe2a54a13c1c553e67cd9fe1
    Former-commit-id: 039856cd7a84dd6fc03fb67748a2320d9262baf4

--- landing_page/public/assets/js/clickhandlers.js
+++ landing_page/public/assets/js/clickhandlers.js
@@ -2,20 +2,26 @@
 $( document ).ready(function() {
   var expanded = $(".navbar-toggle").attr("aria-expanded")
   if (expanded==="true") {
-    $(".navbar").css("padding-bottom", "18rem")
+    $(".navbar").css("padding-bottom", "22rem")
   } else {
     $(".navbar").css("padding-bottom", "0rem")
   }
 });
 
-
-
 $(".navbar-toggle").click(function() {
   var expanded = $(".navbar-toggle").attr("aria-expanded")
-  console.log("EXPANDED", expanded)
   if (expanded==="true") {
       $(".navbar").css("padding-bottom", "0rem")
   } else {
-    $(".navbar").css("padding-bottom", "18rem")
+    $(".navbar").css("padding-bottom", "22rem")
   }
+})
+
+$("#find-out-more").click(function(){
+  $("#welcome-text-container").css("display", "block")
+  $(".google-form").css("display", "block")
+  $("#find-out-more").text("Welcome!")
+  $("#find-out-more").css("color", "#ffb366")
+  $(".learn-more").css("display", "none")
+
 })
