2301dab20e80f286fc609b2fcfc991f582742f20
Spiro Pantazatos <spiropan@gmail.com>
2019-08-07 13:43:56 +0300

    tweak build and master-template parameters

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
+  ParameterKey=MaxSize,UsePreviousValue=tru2
+/web/flask_brain_db/xnat_c9.cf2
 #!/usr/bin/env bash
 
 set -e
 
 REV_TAG=$(git log -1 --pretty=format:%h)
 
+cd ./cookiecutter_mbam
+
 echo "Building image..."
 docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO}:${REV_TAG} .
-docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO}:latest .
 
 echo "Pushing image"
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO}:${REV_TAG}
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO}:latest
 
 echo "Updating CFN"
 aws cloudformation update-stack --stack-name ${STACK_NAME} --use-previous-template --capabilities CAPABILITY_IAM \
@@ -19,4 +21,5 @@ aws cloudformation update-stack --stack-name ${STACK_NAME} --use-previous-templa
   ParameterKey=EnvironmentName,UsePreviousValue=true \
   ParameterKey=InstanceType,UsePreviousValue=true \
   ParameterKey=MakeVPCPublic,UsePreviousValue=true \
-  ParameterKey=MaxSize,UsePreviousValue=true
+  ParameterKey=DesiredCapacity,UsePreviousValue=true \
+  ParameterKey=MaxSize,UsePreviousValue=true \
