a048c3dfcea1f094b145ee5138c7a46feb34564b
Barsha Maharjan <Barsha@Barshas-MacBook-Air.local>
2019-12-08 17:57:35 -0500

    navbar and form css fixes

--- cookiecutter_mbam/assets/js/script.js
+++ cookiecutter_mbam/assets/js/script.js
@@ -4,16 +4,30 @@
 //})
 var spiro=1;
 
-var scroll_pos = 0;
 
-$(document.body).scroll(function() {
-	   scroll_pos = $(this).scrollTop();
-	   if(scroll_pos > 370) {
-	       // alert("scroll_pos > 850")
+
+$(document).ready(function() {  
+	  if ($('#detect-this').length) {
 	       $(".navbar").css('background', '#193b48');
-	   } else {
-	       $(".navbar").css('background', 'transparent');
-	   }
+	 }
+	 else {
+	 	var scroll_pos = 0;
+		$(document.body).scroll(function() {
+			   scroll_pos = $(this).scrollTop();
+			   if(scroll_pos > 370) {
+			       // alert("scroll_pos > 850")
+			       $(".navbar").css('background', '#193b48');
+			   } else {
+			       $(".navbar").css('background', 'transparent');
+			   }
+
+		});
+	 }
 
 });
+
+
+
+
+
 
