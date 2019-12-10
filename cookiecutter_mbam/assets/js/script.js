198e5e46a4549dcbbd9869c311e6796f1d19b4af
Barsha Maharjan <Barsha@Barshas-MacBook-Air.local>
2019-12-10 01:32:05 -0500

    landing page responsiveness

--- cookiecutter_mbam/assets/js/script.js
+++ cookiecutter_mbam/assets/js/script.js
@@ -7,21 +7,20 @@ var spiro=1;
 
 
 $(document).ready(function() {  
-	  if ($('#detect-this').length) {
-	       $(".navbar").css('background', '#193b48');
-	 }
-	 else {
-	 	var scroll_pos = 0;
+	 if ($('#detect-this').length) {
+	    var scroll_pos = 0;
 		$(document.body).scroll(function() {
 			   scroll_pos = $(this).scrollTop();
 			   if(scroll_pos > 370) {
-			       // alert("scroll_pos > 850")
 			       $(".navbar").css('background', '#193b48');
 			   } else {
 			       $(".navbar").css('background', 'transparent');
 			   }
 
 		});
+	 }
+	 else {
+		$(".navbar").css('background', '#193b48');
 	 }
 
 });
