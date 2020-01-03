60007ec1e1f6140695b643d6511acba450040d61
Katie Surrence <tiburona@gmail.com>
2020-01-02 22:02:35 -0500

    fix bugs in docker start script

--- build/start_docker.sh
+++ build/start_docker.sh
@@ -7,16 +7,18 @@ cp -r ./build/docker/* .
 
 cp ./build/docker/web/Dockerfile .
 
-if ["$env" == "test"]; then
+if [ "$env" == "test" ]; then
     sudo /etc/init.d/apache2 stop
     sudo service mysql stop
     docker-compose up -d web
     sleep 5
-elif ["$env" == "server"]; then
-    echo $env
+elif [ "$env" == "server" ]; then
+    echo "$env"
+else
+    echo hello
 fi
 
-dockerDirs=(jatos, mysql, nginx, web)
+dockerDirs=(jatos mysql nginx web)
 for dir in "${dockerDirs[@]}"
 do
     rm -rf ./"${dir}"
