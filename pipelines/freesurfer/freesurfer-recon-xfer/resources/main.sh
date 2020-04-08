809fdc002c35317d71829b2f3be491c9182dc18d
Katie Surrence <tiburona@gmail.com>
2020-04-08 10:07:46 -0400

    changed freesurfer to upload all files

--- pipelines/freesurfer/freesurfer-recon-xfer/resources/main.sh
+++ pipelines/freesurfer/freesurfer-recon-xfer/resources/main.sh
@@ -25,14 +25,16 @@ echo ${args[@]}
 
 recon-all "${args[@]}"
 
+
 echo "zipping up outputs"
-pushd /output/currsub/
-zip -r $(popd)/stats.zip ./stats/
-zip -r $(popd)/mri.zip ./mri/aseg.mgz
-zip -r $(popd)/surf.zip ./surf/{??.pial,??.sphere.reg,??.thickness,??.volume,??.area,??.sulc,??.curv,??.avg_curv}
-popd
+
+for SUBDIR in label mri scripts stats surf touch
+do
+	zip -r "/output/currsub/$SUBDIR.zip" "/output/currsub/$SUBDIR"
+done
 
 echo "uploading recon output"
 
 python3 upload.py $2 $XNAT_USER $XNAT_PASS $3
+
 
